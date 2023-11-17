2c6edb4b85e897bd297ab94264ce48ec0f11fbb2
Eric Andrade <e.andrade5f@gmail.com>
2023-11-17 12:37:25 -0300

    fix: fixes error on parsing of input lines due to wrong substring ranges

--- frontend/app.js
+++ frontend/app.js
@@ -19,10 +19,10 @@ function getParsedInputArray(inputArray){
     const parsedInputArray = inputArray.map(inputLine => {
         const entry = {
             type: inputLine.substring(0,1),
-            date: inputLine.substring(2,26),
+            date: inputLine.substring(1,26),
             product: inputLine.substring(26,56),
-            value: inputLine.substring(57,66),
-            vendor: inputLine.substring(67, 86)
+            value: inputLine.substring(56,66),
+            vendor: inputLine.substring(66, 86)
         }
         return entry
     });
