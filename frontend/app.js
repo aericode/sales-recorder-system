c71b3a775054e8232fd4edc4b6b6d0c28da663d1
Eric Andrade <e.andrade5f@gmail.com>
2023-11-17 12:21:42 -0300

    feat: parsing input lines

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,7 @@
 function handleButtonClick() {
     const inputString = document.getElementById('userInput').value
     const inputArray = getInputArray(inputString)
+    console.log(getParsedInputArray(inputArray))
 }
 
 function getInputArray(inputString){
@@ -9,4 +10,15 @@ function getInputArray(inputString){
 }
 
 function getParsedInputArray(inputArray){
+    const parsedInputArray = inputArray.map(inputLine => {
+        const entry = {
+            type: inputLine.substring(0,1),
+            date: inputLine.substring(2,26),
+            product: inputLine.substring(26,56),
+            value: inputLine.substring(57,66),
+            vendor: inputLine.substring(67, 86)
+        }
+        return entry
+    });
+    return parsedInputArray
 }
